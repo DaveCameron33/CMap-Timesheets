93f81f5ab5380bf1154127a3a86d80216a2edb0a
Dave Cameron <83275112+DavidCameron33@users.noreply.github.com>
2024-12-06 11:31:37 +0000

    DataTable created; DataTable linked to DataGridView; DataGridView 'matched'; UnitTest project added;

--- CMap Timesheets/Form1.Designer.cs
+++ CMap Timesheets/Form1.Designer.cs
@@ -34,21 +34,22 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.grpEntry = new System.Windows.Forms.GroupBox();
             this.dgvEntry = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
             this.colUser = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colProject = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colDescription = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colHours = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.btnAdd = new System.Windows.Forms.Button();
-            this.grpEntries = new System.Windows.Forms.GroupBox();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.dgvAllEntries = new System.Windows.Forms.DataGridView();
+            this.colTotalHours = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.grpButtons = new System.Windows.Forms.GroupBox();
             this.btnExport = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
+            this.grpEntries = new System.Windows.Forms.GroupBox();
+            this.dgvAllEntries = new System.Windows.Forms.DataGridView();
             this.grpEntry.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvEntry)).BeginInit();
+            this.grpButtons.SuspendLayout();
             this.grpEntries.SuspendLayout();
-            this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvAllEntries)).BeginInit();
             this.SuspendLayout();
             // 
@@ -78,7 +79,8 @@
             this.colDate,
             this.colProject,
             this.colDescription,
-            this.colHours});
+            this.colHours,
+            this.colTotalHours});
             dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
             dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -102,8 +104,19 @@
             this.dgvEntry.RowTemplate.DefaultCellStyle.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
             this.dgvEntry.RowTemplate.Height = 40;
             this.dgvEntry.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvEntry.Size = new System.Drawing.Size(1033, 91);
+            this.dgvEntry.Size = new System.Drawing.Size(1024, 91);
             this.dgvEntry.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnAdd.Location = new System.Drawing.Point(1039, 34);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 91);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
             // 
             // colUser
             // 
@@ -134,53 +147,60 @@
             // 
             // colHours
             // 
-            this.colHours.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
             this.colHours.HeaderText = "Hours Worked";
             this.colHours.Name = "colHours";
-            this.colHours.Width = 124;
             // 
-            // btnAdd
+            // colTotalHours
             // 
-            this.btnAdd.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btnAdd.Location = new System.Drawing.Point(1048, 34);
-            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(91, 91);
-            this.btnAdd.TabIndex = 2;
-            this.btnAdd.Text = "Add";
-            this.btnAdd.UseVisualStyleBackColor = true;
+            this.colTotalHours.HeaderText = "Total Hours for the Day";
+            this.colTotalHours.Name = "colTotalHours";
+            this.colTotalHours.Visible = false;
+            // 
+            // grpButtons
+            // 
+            this.grpButtons.Controls.Add(this.btnExit);
+            this.grpButtons.Controls.Add(this.btnExport);
+            this.grpButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.grpButtons.Location = new System.Drawing.Point(0, 422);
+            this.grpButtons.Name = "grpButtons";
+            this.grpButtons.Size = new System.Drawing.Size(1154, 81);
+            this.grpButtons.TabIndex = 11;
+            this.grpButtons.TabStop = false;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnExport.Location = new System.Drawing.Point(3, 22);
+            this.btnExport.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(90, 56);
+            this.btnExport.TabIndex = 10;
+            this.btnExport.Text = "Export to CSV";
+            this.btnExport.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnExit.Location = new System.Drawing.Point(1060, 22);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(91, 56);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
             // 
             // grpEntries
             // 
-            this.grpEntries.Controls.Add(this.tableLayoutPanel1);
+            this.grpEntries.Controls.Add(this.dgvAllEntries);
             this.grpEntries.Dock = System.Windows.Forms.DockStyle.Fill;
             this.grpEntries.Location = new System.Drawing.Point(0, 140);
             this.grpEntries.Margin = new System.Windows.Forms.Padding(8);
             this.grpEntries.Name = "grpEntries";
             this.grpEntries.Padding = new System.Windows.Forms.Padding(15);
-            this.grpEntries.Size = new System.Drawing.Size(1154, 363);
-            this.grpEntries.TabIndex = 2;
+            this.grpEntries.Size = new System.Drawing.Size(1154, 282);
+            this.grpEntries.TabIndex = 12;
             this.grpEntries.TabStop = false;
             this.grpEntries.Text = "Current Entries";
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 81.96585F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.017074F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.017074F));
-            this.tableLayoutPanel1.Controls.Add(this.btnExit, 2, 1);
-            this.tableLayoutPanel1.Controls.Add(this.dgvAllEntries, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.btnExport, 1, 1);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(15, 34);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 65F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1124, 314);
-            this.tableLayoutPanel1.TabIndex = 4;
             // 
             // dgvAllEntries
             // 
@@ -193,7 +213,6 @@
             this.dgvAllEntries.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle3;
             this.dgvAllEntries.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dgvAllEntries.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.tableLayoutPanel1.SetColumnSpan(this.dgvAllEntries, 3);
             dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
             dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -204,36 +223,14 @@
             this.dgvAllEntries.DefaultCellStyle = dataGridViewCellStyle4;
             this.dgvAllEntries.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgvAllEntries.Enabled = false;
-            this.dgvAllEntries.Location = new System.Drawing.Point(4, 5);
+            this.dgvAllEntries.Location = new System.Drawing.Point(15, 34);
             this.dgvAllEntries.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dgvAllEntries.MultiSelect = false;
             this.dgvAllEntries.Name = "dgvAllEntries";
             this.dgvAllEntries.RowHeadersVisible = false;
-            this.dgvAllEntries.Size = new System.Drawing.Size(1116, 239);
-            this.dgvAllEntries.TabIndex = 6;
+            this.dgvAllEntries.Size = new System.Drawing.Size(1124, 233);
+            this.dgvAllEntries.TabIndex = 7;
             this.dgvAllEntries.TabStop = false;
-            // 
-            // btnExport
-            // 
-            this.btnExport.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btnExport.Location = new System.Drawing.Point(927, 254);
-            this.btnExport.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.btnExport.Name = "btnExport";
-            this.btnExport.Size = new System.Drawing.Size(91, 55);
-            this.btnExport.TabIndex = 3;
-            this.btnExport.Text = "Export to CSV";
-            this.btnExport.UseVisualStyleBackColor = true;
-            // 
-            // btnExit
-            // 
-            this.btnExit.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btnExit.Location = new System.Drawing.Point(1029, 254);
-            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(91, 55);
-            this.btnExit.TabIndex = 7;
-            this.btnExit.Text = "Exit";
-            this.btnExit.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -241,6 +238,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1154, 503);
             this.Controls.Add(this.grpEntries);
+            this.Controls.Add(this.grpButtons);
             this.Controls.Add(this.grpEntry);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
@@ -248,8 +246,8 @@
             this.Text = "CMap Timesheet Test";
             this.grpEntry.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgvEntry)).EndInit();
+            this.grpButtons.ResumeLayout(false);
             this.grpEntries.ResumeLayout(false);
-            this.tableLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgvAllEntries)).EndInit();
             this.ResumeLayout(false);
 
@@ -259,17 +257,18 @@
 
         private System.Windows.Forms.GroupBox grpEntry;
         private System.Windows.Forms.DataGridView dgvEntry;
+        private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.DataGridViewTextBoxColumn colUser;
         private System.Windows.Forms.DataGridViewTextBoxColumn colDate;
         private System.Windows.Forms.DataGridViewTextBoxColumn colProject;
         private System.Windows.Forms.DataGridViewTextBoxColumn colDescription;
         private System.Windows.Forms.DataGridViewTextBoxColumn colHours;
-        private System.Windows.Forms.Button btnAdd;
-        private System.Windows.Forms.GroupBox grpEntries;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTotalHours;
+        private System.Windows.Forms.GroupBox grpButtons;
         private System.Windows.Forms.Button btnExit;
-        private System.Windows.Forms.DataGridView dgvAllEntries;
         private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.GroupBox grpEntries;
+        private System.Windows.Forms.DataGridView dgvAllEntries;
     }
 }
 
--- CMap Timesheets/Form1.cs
+++ CMap Timesheets/Form1.cs
@@ -12,9 +12,35 @@ namespace CMap_Timesheets
 {
     public partial class Form1 : Form
     {
+
+        DataTable dtTimesheetEntries;
+
         public Form1()
         {
             InitializeComponent();
+
+            /// Create DataTable for in-memory storage
+            dtTimesheetEntries = new DataTable();
+            dtTimesheetEntries.Columns.Add("colUser", typeof(string));
+            dtTimesheetEntries.Columns.Add("colDate", typeof(DateTime));
+            dtTimesheetEntries.Columns.Add("colProject", typeof(string));
+            dtTimesheetEntries.Columns.Add("colDescription", typeof(string));
+            dtTimesheetEntries.Columns.Add("colHours", typeof(decimal));
+            dtTimesheetEntries.Columns.Add("colTotalHours", typeof(decimal));
+
+            /// Link DataTable to readonly DataGridView
+            dgvAllEntries.DataSource = dtTimesheetEntries;
+
+            /// Adjust columns so they match the Entry
+            foreach (DataGridViewColumn col in dgvEntry.Columns)
+            {
+                dgvAllEntries.Columns[col.Name].HeaderText = col.HeaderText;
+                dgvAllEntries.Columns[col.Name].AutoSizeMode = col.AutoSizeMode;
+                if (col.AutoSizeMode == DataGridViewAutoSizeColumnMode.NotSet)
+                {
+                    dgvAllEntries.Columns[col.Name].Width = col.Width;
+                }
+            }
         }
     }
 }
