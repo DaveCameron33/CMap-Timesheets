6795101e892666bb43fa913ba76d39a2be7bad18
Dave Cameron <83275112+DavidCameron33@users.noreply.github.com>
2024-12-09 16:20:09 +0000

    AddRowToDataTable method added to UnitTest + tested successfully;

--- CMap Timesheets.Tests/UnitTest1.cs
+++ CMap Timesheets.Tests/UnitTest1.cs
@@ -1,14 +1,70 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace CMap_Timesheets.Tests
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+
+        private Form1_Test form1;
+        private DataGridView dgvEntry_Test;
+        private DataTable dtTimesheetEntries;
+
+        public UnitTest1()
         {
+            dgvEntry_Test = new DataGridView();
+            dgvEntry_Test.Columns.Add(new DataGridViewColumn() { Name = "colUser", HeaderText = "User Name", AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells, CellTemplate = new DataGridViewTextBoxCell() });
+            dgvEntry_Test.Columns.Add(new DataGridViewColumn() { Name = "colDate", HeaderText = "Date", AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells, CellTemplate = new DataGridViewTextBoxCell() });
+            dgvEntry_Test.Columns.Add(new DataGridViewColumn() { Name = "colProject", HeaderText = "Project", AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells, CellTemplate = new DataGridViewTextBoxCell() });
+            dgvEntry_Test.Columns.Add(new DataGridViewColumn() { Name = "colDescription", HeaderText = "Description", AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill, CellTemplate = new DataGridViewTextBoxCell() });
+            dgvEntry_Test.Columns.Add(new DataGridViewColumn() { Name = "colHours", HeaderText = "Hours Worked", AutoSizeMode = DataGridViewAutoSizeColumnMode.NotSet, CellTemplate = new DataGridViewTextBoxCell() });
+            dgvEntry_Test.Columns.Add(new DataGridViewColumn() { Name = "colTotalHours", HeaderText = "Total Hours for the Day", AutoSizeMode = DataGridViewAutoSizeColumnMode.NotSet, CellTemplate = new DataGridViewTextBoxCell(), Visible = false });
+
+            dtTimesheetEntries = new DataTable();
+            dtTimesheetEntries.Columns.Add("colUser", typeof(string));
+            dtTimesheetEntries.Columns.Add("colDate", typeof(DateTime));
+            dtTimesheetEntries.Columns.Add("colProject", typeof(string));
+            dtTimesheetEntries.Columns.Add("colDescription", typeof(string));
+            dtTimesheetEntries.Columns.Add("colHours", typeof(decimal));
+            dtTimesheetEntries.Columns.Add("colTotalHours", typeof(decimal));
+
+        }
+
+        [TestMethod]
+        public void DataIsAddedToDataTable()
+        {
+            /// Arrange
+            form1 = new Form1_Test();
+
+            dgvEntry_Test.Rows.Add("Dave", "06/12/2024", "My First Project", "Setting up for the first time", 1d);
+
+            /// Act
+            form1.AddRowToDataTable(dtTimesheetEntries, dgvEntry_Test);
+
+            /// Assert
+            Assert.IsTrue(dtTimesheetEntries.Rows.Count == 1);
+
+        }
+    }
+
+    class Form1_Test: Form1
+    {
+        public void AddRowToDataTable(DataTable dataTable, DataGridView dataGridView)
+        {
+            DataGridViewCellCollection cells = dataGridView.Rows[0].Cells;
+
+            DataRow newRow = dataTable.NewRow();
+            foreach (DataGridViewColumn col in dataGridView.Columns.Cast<DataGridViewColumn>().Where(c => c.Visible == true))
+            {
+                newRow[col.Name] = cells[col.Name].FormattedValue;
+            }
+
+            dataTable.Rows.Add(newRow);
         }
     }
 }
